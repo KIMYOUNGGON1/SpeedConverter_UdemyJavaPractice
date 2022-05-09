17444868995620479cd6cb7c481ea461c92d72d1
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-05-10 08:59:39 +0900

    2022-05-10 Uedmy Java 강의 내용

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,3 +1,11 @@
 public class SpeedConverter {
-    
+
+    public static long toMilesPerHour(double kilometerPerHour) {
+        if (kilometerPerHour < 0) {
+            return -1;
+        }
+
+        long milePerHour = Math.round(kilometerPerHour / 1.609);
+
+    }
 }
